68fd931bbe51d4eda5d9c3c8d83f5790af9d44e1
dripminister <95714820+dripminister@users.noreply.github.com>
2022-11-03 20:55:49 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ChatDisplay from '../components/ChatDisplay'
 import Navbar from '../components/Navbar'
 import TextInput from '../components/TextInput'
-import { auth } from '../firebase'
 
 export default function Home() {
 
