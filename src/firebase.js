0ff7f5cf152d1100bce92cb729e27f9cbdf0309b
dripminister <95714820+dripminister@users.noreply.github.com>
2022-11-03 19:41:42 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,7 @@ import { getStorage } from "firebase/storage"
 import { initializeApp } from "firebase/app"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDdW2J3Bcv_3B-sI8RWJ1MUhs9pxeqLlPo",
-  authDomain: "open-chat-45917.firebaseapp.com",
-  projectId: "open-chat-45917",
-  storageBucket: "open-chat-45917.appspot.com",
-  messagingSenderId: "129145482216",
-  appId: "1:129145482216:web:72d02f7713e410b7c2d25d"
+  //Setup your firebase app and put your config here
 }
 
 
